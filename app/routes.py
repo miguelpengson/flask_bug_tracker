e615b939d9680514416c0064fff77f26a514c6bd
miguelpengson <miguelpengson@gmail.com>
2020-03-07 07:21:47 +0800

    added shell contex processor, problems add form data into the database

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from app import app, db
 from app.models import Tracker
 from app.forms import TrackerForm
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Tracker': Tracker}
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -13,4 +17,12 @@ def index():
 @app.route("/bug/new", methods=['GET', 'POST'])
 def new_bug():
     form = TrackerForm()
+    if form.validate_on_submit():
+        track = Tracker(title=form.title.data, content=form.content.data)
+        db.session.add(track)
+        db.session.commit()
+        flash('Your Bug tracker has been created!', 'success')
+        return redirect(url_for('index'))
     return render_template('new_bug.html', title='New Bug Report', form=form, legend='New Bug')
+
+
