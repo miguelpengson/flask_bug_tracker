d41a02c789060cf7372bb899ab721096f0235790
miguelpengson <miguelpengson@gmail.com>
2020-03-15 13:23:41 +0800

    update and delete bug added, fixed flashed messages

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from app import app, db
 from app.models import Tracker
 from app.forms import TrackerForm
@@ -29,3 +29,26 @@ def new_bug():
 def bug(track_id):
     bug = Tracker.query.get_or_404(track_id)
     return render_template('bug.html', title=bug.title, bug=bug)
+
+@app.route("/bug/<track_id>/update", methods=['GET', 'POST'])
+def update_bug(track_id):
+    bug = Tracker.query.get_or_404(track_id)
+    form = TrackerForm()
+    if form.validate_on_submit():
+        bug.title = form.title.data
+        bug.content = form.content.data
+        db.session.commit()
+        flash('Your bug has been updated!', 'success')
+        return redirect(url_for('index', track_id=track_id))
+    elif request.method == 'GET':
+        form.title.data = bug.title
+        form.content.data = bug.content
+    return render_template('new_bug.html', title='Update Bug', form=form, legend='Update Bug')
+
+@app.route("/note/<track_id>/delete", methods=['POST'])
+def delete_bug(track_id):
+    bug = Tracker.query.get_or_404(track_id)
+    db.session.delete(bug)
+    db.session.commit()
+    flash('Your note has been deleted!', 'success')
+    return redirect(url_for('index'))
