f41638b0b4db916c9bea2706f777cd2a784e5417
miguelpengson <miguelpengson@gmail.com>
2020-03-19 19:38:23 +0800

    testing the database

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    trackers = db.relationship('Tracker', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f"User {self.username}"
@@ -16,6 +17,7 @@ class Tracker(db.Model):
     title = db.Column(db.String(140), nullable=False)
     content = db.Column(db.String(300), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f"Bug('{self.id}', '{self.title}', '{self.content}', {self.date_created})"
