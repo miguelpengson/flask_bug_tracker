0e2943786ee6e851e62e918d87bfb5832fe89479
miguelpengson <miguelpengson@gmail.com>
2020-03-22 18:30:47 +0800

    add requirements.txt

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,37 @@
 from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
 from app import app, db
-from app.models import Tracker
-from app.forms import TrackerForm
+from app.models import User, Tracker
+from app.forms import LoginForm, TrackerForm
 
 @app.route("/")
 @app.route("/index")
 def index():
     track = Tracker.query.all()
     return render_template('index.html', track=track)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password!')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route("/bug/new", methods=['GET', 'POST'])
